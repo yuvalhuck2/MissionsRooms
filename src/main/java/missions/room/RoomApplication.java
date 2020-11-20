4278418c5432c93cf8dfd5f94b538ed08e34da05
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-11-20 12:04:20 +0200

    add mail sender

--- src/main/java/missions/room/RoomApplication.java
+++ src/main/java/missions/room/RoomApplication.java
@@ -2,20 +2,18 @@ package missions.room;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
+//@RestController
+@EnableJpaRepositories("Pack_Try")
+@EntityScan("Pack_Try")
 public class RoomApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RoomApplication.class, args);
     }
 
-    @GetMapping("/hello")
-    public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
 }
