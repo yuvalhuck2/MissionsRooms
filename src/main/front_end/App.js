ad0fd6830171e2e46c0f93362369d89839923225
tal773 <tal9@post.bgu.ac.il>
2020-11-24 15:36:38 +0200

    added call for /hello

--- src/main/front_end/App.js
+++ src/main/front_end/App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert  } from 'react-native';
 
 export default function App() {
+  let onPressLearnMore = () => fetch('http://localhost:8080/hello', {
+      method: 'GET'
+  }).then((response) => response.json()).then((res) => {
+    Alert.alert(res);
+  }).catch((err) => Alert.alert(err.toString()));
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+          onPress={onPressLearnMore}
+          title="Learn More"
+          color="#841584"
+      />
       <StatusBar style="auto" />
     </View>
   );
