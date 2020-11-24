1bf88b75cde428bb374973750b7e4b12e30983f0
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-11-24 12:22:03 +0200

    connection to DB working

--- src/main/java/Pack_Try/User.java
+++ src/main/java/Pack_Try/User.java
@@ -6,7 +6,8 @@ import javax.persistence.*;
 public class User {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
     private Integer id;
 
     private String name;
