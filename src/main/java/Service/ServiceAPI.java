c91abe11403e69dffd94a9068263897b7a98feeb
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-11-27 14:50:39 +0200

    finish student requirements API

--- src/main/java/Service/ServiceAPI.java
+++ src/main/java/Service/ServiceAPI.java
@@ -86,7 +86,7 @@ public class ServiceAPI {
     }
 
     /**
-     * 3.6.2.1 - answer open question mission
+     * req 3.6.2.1 - answer open question mission
      * @param auth - authentication object
      * @param roomId - room id
      * @param answer - verbal answer of the open question
@@ -98,10 +98,106 @@ public class ServiceAPI {
         throw new NotImplementedException();
     }
 
+
+    //TODO talk about discussion mission
     /**
-     * 3.6.2.2 - answer open discussion mission
+     * req 3.6.2.2 - answer open discussion mission
      * @return if the answer was accepted successfully
      */
+
+    /**
+     * req 3.6.2.3 - answer deterministic question mission
+     * @param auth - authentication object
+     * @param roomId - room id
+     * @param answer - answer for the question
+     * @return if the answer was correct
+     */
+    public Response<Boolean> answerDeterministicQuestion(Auth auth,int roomId,String answer){
+        throw new NotImplementedException();
+    }
+
+
+    /**
+     * req 3.6.2.4.1 - fill true and lie sentences
+     * @param auth - authentication object
+     * @param roomId - room id
+     * @param trueSentence - the correct sentence about the student
+     * @param falseSentence - incorrect sentence about the student
+     * @return if the sentences were added to the mission
+     */
+    public Response<Boolean> answerTrueLieQuestion(Auth auth, int roomId,String trueSentence, String falseSentence){
+        throw new NotImplementedException();
+    }
+
+
+    /**
+     * req 3.6.2.4.2 - answer true lie sentence
+     * @param auth - authentication object
+     * @param roomId - room id
+     * @param mail - th mail of the
+     * @param trueAnswer - true if the sentence the student think the first sentence is correct
+     * @return if the answer was correct
+     */
+    public Response<Boolean> answerTrueLieQuestion(Auth auth, int roomId,String mail, boolean trueAnswer){
+        throw new NotImplementedException();
+    }
+
+
+    /**
+     * req3.6.2.5 - answer story mission
+     * @param auth - authentication object
+     * @param roomId - room id
+     * @param sentence - the next sentence to add to the story
+     * @return - the whole story after adding the next sentence
+     */
+    public Response<String> answerStoryMission(Auth auth,int roomId, String sentence){
+        throw new NotImplementedException();
+    }
+
+
+    /**
+     * req 3.6.2.6 - answer trivia mission( american question)
+     * @param auth - authentication object
+     * @param roomId - room id
+     * @param answerNumber - the number of the answer that the students answers
+     * @return - the correct answer number
+     */
+    public Response<Integer> answerTriviaQuestion(Auth auth,int roomId, int answerNumber){
+        throw new NotImplementedException();
+    }
+
+    /**
+     * TODO 3.6.2.7 talk about when the system draw the in charge
+     */
+
+
+    /**
+     * req 3.7 - add suggestion
+     * @param auth - authentication object
+     * @param suggestion - suggestion to a mission the student want to send
+     * @return if the suggestion was added successfully
+     */
+    public Response<Boolean> addSuggestion(Auth auth, String suggestion){
+        throw new NotImplementedException();
+    }
+
+    /**
+     * req 3.8 - reset password
+     * @param auth - authentication object
+     * @return if an email was sent with verification code
+     */
+    public Response<Boolean> resetPassword(Auth auth){
+        throw new NotImplementedException();
+    }
+
+
+
+
+
+
+
+
+
 
 
 
