6149d3d8270e7fd0016b5561e31598254698a7ac
Roy Levy <roy4@post.bgu.ac.il>
2020-12-04 12:38:27 +0200

    IT object

--- src/main/java/Domain/Student.java
+++ src/main/java/Domain/Student.java
@@ -1,11 +1,16 @@
 package Domain;
 
-import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.MapKeyColumn;
+import javax.persistence.OneToMany;
 
 @Entity
 @Inheritance(strategy = InheritanceType.JOINED)
