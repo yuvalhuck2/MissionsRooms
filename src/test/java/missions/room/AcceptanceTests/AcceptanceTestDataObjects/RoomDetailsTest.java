55d4699ff69928c1f8476728c3fd85e6efc2cf9e
yuvalhuck1 <sabay@post.bgu.ac.il>
2020-12-24 23:13:18 +0200

    almost finish add room

---
+++ src/test/java/missions/room/AcceptanceTests/AcceptanceTestDataObjects/RoomDetailsTest.java
@@ -0,0 +1,9 @@
+package missions.room.AcceptanceTests.AcceptanceTestDataObjects;
+
+import java.util.List;
+
+public class RoomDetailsTest {
+
+    private String roomId;
+    private List<MissionDetailsTest> unfinishedMissions;
+}
