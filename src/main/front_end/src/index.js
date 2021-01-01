1dd433b7466a460081a4563357fac91378bf4b8e
tal773 <tal9@post.bgu.ac.il>
2021-01-01 11:07:02 +0200

    ITScreen ugly - only GUI

--- src/main/front_end/src/index.js
+++ src/main/front_end/src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import { LoginScreen, RegisterScreen } from './components/screens';
+import { LoginScreen, RegisterScreen, ITScreen } from './components/screens';
 
 const Stack = createStackNavigator();
 
 const App = () =>{
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Register">
+      <Stack.Navigator initialRouteName="IT">
         <Stack.Screen name="Register" component={RegisterScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="IT" component={ITScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
