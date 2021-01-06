6b2d0894beb950bcaa39f637a0e3b02a69dc3aa3
tal773 <tal9@post.bgu.ac.il>
2021-01-06 18:32:58 +0200

    redux in upload csv screen

--- src/main/front_end/src/components/screens/UploadCsvScreen.js
+++ src/main/front_end/src/components/screens/UploadCsvScreen.js
@@ -9,7 +9,7 @@ import Header from '../common/Header';
 import TextInput from '../common/TextInput';
 import * as DocumentPicker from 'expo-document-picker';
 import { Platform } from 'react-native';
-
+import {onRestart, onPickFile, onSendFiles } from '../../actions'
 
 const {
     header,
@@ -24,79 +24,27 @@ const {
 
 const DeviceWidth = Dimensions.get('window').width;
 class UploadCsvForm extends Component {
-    files = []
     constructor(...args) {
         super(...args);
-        this.state = { text: '' };
+        this.onPickFile = this.onPickFile.bind(this)
+        this.onRestart = this.onRestart.bind(this)
+        this.onSendFiles = this.onSendFiles.bind(this)
     }
 
-    async onRestart(){
-        this.file = []
-        this.setState({text: ""})
+    onRestart(){
+        this.props.onRestart()
     }
 
-    async onPickFile() {
-        let res = DocumentPicker.getDocumentAsync({
-            type: 'text/comma-separated-values',
-            multiple: true,
-            copyToCacheDirectory: true
-        }).then(res => {
-            if (res != undefined && res != null && res.type != 'cancel') {
-                this.files.push(res)
-                this.setState({ text: this.state.text + " " + res.name })
-            }
-        }).catch(err => alert("Document Picker Error"));
+    onPickFile() {
+        this.props.onPickFile()
     }
 
     async onSendFiles() {
-        const formData = new FormData();
-        if (this.files.length != 4){
-            alert(file_number_error)
-            return
-        }
-        this.files.forEach(file => {
-            formData.append("files", {
-                uri: Platform.OS === 'android' ? file.uri : file.uri.replace('file://', ''),
-                type: "*/*",
-                name: file.name
-            });
-        });  
-        const xhr = new XMLHttpRequest();
-        // 2. open request
-        xhr.open('POST', 'http://192.168.1.14:8080/uploadCsv?token=abc');
-        //Send the proper header information along with the request
-        xhr.setRequestHeader('Content-type', 'multipart/form-data');
-        // 3. set up callback for request
-        xhr.onload = () => {
-            const response = JSON.parse(xhr.response);
-            //TODO: parse by code
-            alert(response);
-            // ... do something with the successful response
-        };
-        // 4. catch for request error
-        xhr.onerror = e => {
-            alert('upload failed');
-        };
-        // 4. catch for request timeout
-        xhr.ontimeout = e => {
-            alert('upload timeout');
-        };
-
-
-        xhr.onreadystatechange = function () {//Call a function when the state changes.
-            alert(xhr.responseText);
-        }
-
-        xhr.send(formData);
-
-        this.files = []
-        this.setState({ text: "" })
-
-
+        this.props.onSendFiles({files: this.props.files})
     }
 
     render() {
-        const { email, password } = this.props;
+        const { text } = this.props;
 
         return (
             <KeyboardAwareScrollView contentContainerStyle={styles.wrapper} style={styles.container}>
@@ -104,7 +52,7 @@ class UploadCsvForm extends Component {
                 <View style={styles.row}>
                     <TextInput style={styles.email_label}
                         editable={false}
-                        value={this.state.text}
+                        value={text}
                     />
                     <Button style={styles.button}
                         mode='contained'
@@ -158,8 +106,9 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state) => {
-    return {};
+    const {text, files} = state.IT
+    return {text, files};
 };
 
-export default connect(mapStateToProps, {
-})(UploadCsvForm);
+export default connect(mapStateToProps, 
+    { onRestart, onPickFile, onSendFiles })(UploadCsvForm);
