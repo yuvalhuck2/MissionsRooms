59f9d789d8af8cb074ffc7813e279ad3c70c3b1f
Roy Levy <roy4@post.bgu.ac.il>
2021-01-08 21:43:31 +0200

    error handling

--- src/main/front_end/src/reducers/index.js
+++ src/main/front_end/src/reducers/index.js
@@ -1,16 +1,16 @@
 import { combineReducers } from 'redux';
-import addMissionReducer from './AddMissionReducer';
-import AuthReducer from './AuthReducer'
-import addRoomTemplateReducer from './AddRoomTemlateReducer'
-import addRoomReducer from './AddRoomReducer'
-import ChooseStudentRoom from './ChooseStudentRoomReducer'
-import ITReducer from './ITReducer'
+import addMissionReducer from './addMissionReducer';
+import addRoomReducer from './AddRoomReducer';
+import addRoomTemplateReducer from './addRoomTemlateReducer';
+import AuthReducer from './AuthReducer';
+import ChooseStudentRoom from './ChooseStudentRoomReducer';
+import ITReducer from './ITReducer';
 
 export default combineReducers({
   auth: AuthReducer,
   addMission: addMissionReducer,
   addRoomTemplate: addRoomTemplateReducer,
   addRoom: addRoomReducer,
-  ChooseStudentRoom:ChooseStudentRoom,
+  ChooseStudentRoom: ChooseStudentRoom,
   IT: ITReducer,
 });
