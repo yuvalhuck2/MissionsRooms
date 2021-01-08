2a2a633f68b548b313c8c7804b4b072ee8a0d52f
Roy Levy <roy4@post.bgu.ac.il>
2021-01-08 20:56:28 +0200

    teacher actions errors

--- src/main/front_end/src/actions/TeacherActions.js
+++ src/main/front_end/src/actions/TeacherActions.js
@@ -1,63 +1,65 @@
-import API from '../api/API';
-import { GeneralErrors } from '../locale/locale_heb';
-import * as APIPaths from '../api/APIPaths'
-import * as NavPaths from '../navigation/NavPaths'
-
 import {
-    UPDATE_ERROR_TEMPLATE,
-    UPDATE_ERROR_ROOM,
-    GET_MISSIONS,
-    GET_TEMPLATES,
-    LOGIN_TEACHER,
-    GET_CLASSROOM,
-    CLEAR_STATE,
-  } from '../actions/types';
+  CLEAR_STATE,
+  GET_CLASSROOM,
+  GET_MISSIONS,
+  GET_TEMPLATES,
+  LOGIN_TEACHER,
+  UPDATE_ERROR_ROOM,
+  UPDATE_ERROR_TEMPLATE,
+} from '../actions/types';
+import API from '../api/API';
+import * as APIPaths from '../api/APIPaths';
+import { GeneralErrors } from '../locale/locale_heb';
+import * as NavPaths from '../navigation/NavPaths';
+import { Not_Exist, Success, Wrong_Key } from './OpCodeTypes';
 
-  import {
-    Success,
-  } from './OpCodeTypes';
+const {
+  server_error,
+  wrong_key_error,
+  teacher_not_exists_error,
+} = GeneralErrors;
 
-  const {
-    server_error,
-  }=GeneralErrors
-
-export const passToAddTemplate=({navigation,apiKey})=>{
-    
-    return async (dispatch)=> {
-      dispatch({ type: LOGIN_TEACHER,payload:apiKey});
-      navigation.navigate(NavPaths.addTemplate);
-        try {
-            const res = await API.post(APIPaths.searchMission, {apiKey});
-            res ? checkSearchMissionResponse(res.data, dispatch)
-            : dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
-          } catch (err) {
-            console.log(err);
-            return dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
-          }
-        
-    }
-}
-
-const checkSearchMissionResponse = (data,dispatch) =>{
-    const {reason,value} =data
-    switch (reason) {
-          case Success:
-            return dispatch({ type: GET_MISSIONS, payload: value });
-          default:
-            return dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
-      }
-}
-
-export const passToAddRoom=({navigation,apiKey})=>{
-    
-  return async (dispatch)=> {
-    dispatch({ type: LOGIN_TEACHER,payload:apiKey});
+export const passToAddTemplate = ({ navigation, apiKey }) => {
+  return async (dispatch) => {
+    dispatch({ type: LOGIN_TEACHER, payload: apiKey });
+    navigation.navigate(NavPaths.addTemplate);
     try {
-      const res = await API.post(APIPaths.getClassroom, {apiKey});
-      if(res) {
-        checkGetClassroomResponse(res.data, dispatch,navigation)
-      }
-      else{
+      const res = await API.post(APIPaths.searchMission, { apiKey });
+      res
+        ? checkSearchMissionResponse(res.data, dispatch)
+        : dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
+    } catch (err) {
+      console.log(err);
+      return dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
+    }
+  };
+};
+
+const checkSearchMissionResponse = (data, dispatch) => {
+  const { reason, value } = data;
+  switch (reason) {
+    case Wrong_Key:
+      return dispatch({ type: UPDATE_ERROR_TEMPLATE, wrong_key_error });
+    case Not_Exist:
+      return dispatch({
+        type: UPDATE_ERROR_TEMPLATE,
+        teacher_not_exists_error,
+      });
+    case Success:
+      return dispatch({ type: GET_MISSIONS, payload: value });
+    default:
+      return dispatch({ type: UPDATE_ERROR_TEMPLATE, payload: server_error });
+  }
+};
+
+export const passToAddRoom = ({ navigation, apiKey }) => {
+  return async (dispatch) => {
+    dispatch({ type: LOGIN_TEACHER, payload: apiKey });
+    try {
+      const res = await API.post(APIPaths.getClassroom, { apiKey });
+      if (res) {
+        checkGetClassroomResponse(res.data, dispatch, navigation);
+      } else {
         dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
       }
     } catch (err) {
@@ -65,41 +67,55 @@ export const passToAddRoom=({navigation,apiKey})=>{
       return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
     }
     try {
-      const res = await API.post(APIPaths.searchTemplate, {apiKey});
-        res ? checkSearchTemplatesResponse(res.data, dispatch)
+      const res = await API.post(APIPaths.searchTemplate, { apiKey });
+      res
+        ? checkSearchTemplatesResponse(res.data, dispatch)
         : dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
-      } catch (err) {
-        console.log(err);
-        return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
-      }
-      
+    } catch (err) {
+      console.log(err);
+      return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
+    }
+  };
+};
+
+const checkGetClassroomResponse = (data, dispatch, navigation) => {
+  const { reason, value } = data;
+  switch (reason) {
+    case Wrong_Key:
+      return dispatch({ type: UPDATE_ERROR_TEMPLATE, wrong_key_error });
+    case Not_Exist:
+      return dispatch({
+        type: UPDATE_ERROR_TEMPLATE,
+        teacher_not_exists_error,
+      });
+    case Success:
+      dispatch({ type: GET_CLASSROOM, payload: value });
+      return navigation.navigate(NavPaths.AddRoom);
+    default:
+      return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
   }
-}
+};
 
-const checkGetClassroomResponse = (data,dispatch,navigation) =>{
-  const {reason,value} =data
+const checkSearchTemplatesResponse = (data, dispatch) => {
+  const { reason, value } = data;
   switch (reason) {
-        case Success:
-          dispatch({ type: GET_CLASSROOM, payload: value });
-          return navigation.navigate(NavPaths.AddRoom);
-        default:
-          return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
-    }
-}
+    case Wrong_Key:
+      return dispatch({ type: UPDATE_ERROR_TEMPLATE, wrong_key_error });
+    case Not_Exist:
+      return dispatch({
+        type: UPDATE_ERROR_TEMPLATE,
+        teacher_not_exists_error,
+      });
+    case Success:
+      return dispatch({ type: GET_TEMPLATES, payload: value });
+    default:
+      return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
+  }
+};
 
-const checkSearchTemplatesResponse = (data,dispatch) =>{
-  const {reason,value} =data
-  switch (reason) {
-        case Success:
-          return dispatch({ type: GET_TEMPLATES, payload: value });
-        default:
-          return dispatch({ type: UPDATE_ERROR_ROOM, payload: server_error });
-    }
-}
-
-export const logout=(navigation)=>{
-  return async (dispatch)=> {
-    dispatch({type: CLEAR_STATE})
+export const logout = (navigation) => {
+  return async (dispatch) => {
+    dispatch({ type: CLEAR_STATE });
     return navigation.navigate(NavPaths.loginScreen);
-  }
-}
+  };
+};
