9eacac562a47abf7a4b69bf8b78320f694d9dbc6
yuvalhuck1 <sabay@post.bgu.ac.il>
2021-02-20 11:49:56 +0200

    multi users in room support

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const baseURL = "http://10.0.0.7:8080"
-
+//export const baseURL = "http://132.72.200.67:8080"
 export default axios.create({
     baseURL: baseURL,
   });
