c1da5173c2e0b2fd1d91e59106f20431d2599232
RoyLevy24 <roy4@post.bgu.ac.il>
2021-03-01 22:13:58 +0200

    getting ready for the demo

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 //export const baseURL = "http://132.72.47.211:8080"
-export const baseURL = "http://192.168.1.12:8080"
+export const baseURL = "http://192.168.0.10:8080"
 export default axios.create({
     baseURL: baseURL,
   });
