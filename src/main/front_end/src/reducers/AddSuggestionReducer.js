dfe549a9f8541e736d42b7dd1123a8669ced55da
yuvalhuck1 <sabay@post.bgu.ac.il>
2021-04-04 15:39:15 +0300

    fix bug of add suggestion

--- src/main/front_end/src/reducers/AddSuggestionReducer.js
+++ src/main/front_end/src/reducers/AddSuggestionReducer.js
@@ -1,13 +1,16 @@
 import {
     CLEAR_STATE,
     LOGIN_STUDENT,
-    UPDATE_ERROR,ADD_SUGGESTION,SUGGESTION_CHANGED
+    UPDATE_ERROR_ADD_SUGGESTION,
+    ADD_SUGGESTION,SUGGESTION_CHANGED,
+    WAIT_FOR_ADD_SUGGESTION,
 } from "../actions/types";
 
 const initialState = {
     suggestion:'',
     errorMessage: '',
     apiKey:'',
+    loading: false,
 };
 
 
@@ -15,12 +18,15 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case SUGGESTION_CHANGED:
             return { ...state, suggestion: action.payload };
-        case UPDATE_ERROR:
-            return { ...state, errorMessage: action.payload};
+        case UPDATE_ERROR_ADD_SUGGESTION:
+            alert(action.payload)
+            return { ...state, errorMessage: action.payload, loading: false,};
         case LOGIN_STUDENT:
             return { ...initialState, apiKey: action.payload};
         case ADD_SUGGESTION:
-            return {...state,errorMessage:action.payload};
+            return {...state,errorMessage:''};
+        case WAIT_FOR_ADD_SUGGESTION:
+            return {...state, errorMessage:'', loading: true};
         case CLEAR_STATE:
             return initialState;
         default:
