24cb61bd1403e19e1c5e45d812bfadac35a6fee8
yuvalhuck1 <sabay@post.bgu.ac.il>
2021-04-24 17:42:21 +0300

    without postgres

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 //export const baseURL = "http://132.73.216.240:8080"
 //export const baseURL = "http:///192.168.14.55:8080"
-export const baseURL = "http://132.73.209.45:8080"
+//export const baseURL = "http://132.73.209.45:8080"
+export const baseURL = "http://132.72.47.211:8080"
 export default axios.create({
     baseURL: baseURL,
   });
