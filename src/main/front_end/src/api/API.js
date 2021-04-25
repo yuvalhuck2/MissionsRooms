621699db3c4ec617a3f98a4b98283273c778c820
tal773 <tal9@post.bgu.ac.il>
2021-04-25 18:39:21 +0300

    update points table

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //export const baseURL = "http://132.72.232.60:8080"
-// export const baseURL = "http://132.73.205.248:8080"
-export const baseURL = "http://132.73.47.211:8080"
+ export const baseURL = "http://132.73.205.248:8080"
+//export const baseURL = "http://132.72.47.211:8080"
 export default axios.create({
     baseURL: baseURL,
   });
