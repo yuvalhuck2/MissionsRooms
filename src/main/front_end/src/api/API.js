9ea6596f806be84eaf2006225aead2238602955d
tal773 <tal9@post.bgu.ac.il>
2021-05-29 17:25:03 +0300

    https key

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-export const baseURL = "http://192.168.1.39:8080"
-//export const baseURL = "http:///192.168.14.55:8080"
+//export const baseURL = "https://192.168.1.39:8080"
+export const baseURL = "https:// 192.168.1.18:8443"
 export default axios.create({
     baseURL: baseURL,
   });
