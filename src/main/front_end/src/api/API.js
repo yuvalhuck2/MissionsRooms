af5f7dfa2a5b99d291f0f979f6c38acca09497a6
tal773 <tal9@post.bgu.ac.il>
2021-05-30 12:12:52 +0300

    added https agent to axios

--- src/main/front_end/src/api/API.js
+++ src/main/front_end/src/api/API.js
@@ -1,8 +1,11 @@
 import axios from 'axios'
+import https from 'https';
 //export const baseURL = "https://192.168.1.39:8080"
-export const baseURL = "https:// 192.168.1.18:8443"
+export const baseURL = "https://192.168.1.18:8443"
+const agent = new https.Agent({rejectUnauthorized: false})
 export default axios.create({
     baseURL: baseURL,
+    httpsAgent: agent
   });
 
   // THIS IS FOR CHECKING DNS ADDRESS WHEN RUNNING EXPO ON PHYSICAL DEVICE
